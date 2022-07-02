9750c6d0b1dc31a064e479d8bbf27e11feb4680c
nk7260ynpa <nk7260ynpa@gmail.com>
2022-07-02 11:45:40 +0800

    Add LUNA16_download.sh

---
+++ data/LUNA16_download.sh
@@ -0,0 +1,20 @@
+mkdir -p LUNA16
+
+wget -O LUNA16/annotations.csv      https://zenodo.org/record/3723295/files/annotations.csv?download=1
+wget -O LUNA16/candidates.csv       https://zenodo.org/record/3723295/files/candidates.csv?download=1
+wget -O LUNA16/candidates_V2.zip    https://zenodo.org/record/3723295/files/candidates_V2.zip?download=1
+wget -O LUNA16/evaluationScript.zip https://zenodo.org/record/3723295/files/evaluationScript.zip?download=1
+wget -O LUNA16/sampleSubmission.csv https://zenodo.org/record/3723295/files/sampleSubmission.csv?download=1
+wget -O LUNA16/seg-lungs-LUNA16.zip https://zenodo.org/record/3723295/files/seg-lungs-LUNA16.zip?download=1
+wget -O LUNA16/subset0.zip          https://zenodo.org/record/3723295/files/subset0.zip?download=1
+wget -O LUNA16/subset1.zip          https://zenodo.org/record/3723295/files/subset1.zip?download=1
+wget -O LUNA16/subset2.zip          https://zenodo.org/record/3723295/files/subset2.zip?download=1
+wget -O LUNA16/subset3.zip          https://zenodo.org/record/3723295/files/subset3.zip?download=1
+wget -O LUNA16/subset4.zip          https://zenodo.org/record/3723295/files/subset4.zip?download=1
+wget -O LUNA16/subset5.zip          https://zenodo.org/record/3723295/files/subset5.zip?download=1
+wget -O LUNA16/subset6.zip          https://zenodo.org/record/3723295/files/subset6.zip?download=1
+wget -O LUNA16/subset7.zip          https://zenodo.org/record/3723295/files/subset7.zip?download=1
+wget -O LUNA16/subset8.zip          https://zenodo.org/record/3723295/files/subset8.zip?download=1
+wget -O LUNA16/subset9.zip          https://zenodo.org/record/3723295/files/subset9.zip?download=1
+
+
